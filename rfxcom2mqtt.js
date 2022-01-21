15b8162f84ef7810f0a88af02f6ecdbf15c39d79
legobas <legobas@gmail.com>
2022-01-21 16:10:47 +0100

    config added

--- rfxcom2mqtt.js
+++ rfxcom2mqtt.js
@@ -2,9 +2,8 @@
 
 const mqtt = require('mqtt'); 
 const rfxcom = require('rfxcom');
-const config = require('node-config-yaml').load("./config.yml");
+const config = require('node-config-yaml').load("/app/data/config.yml");
 
-const device = config.rfxcom.device;
 const topic = 'rfxcom2mqtt/devices';
 const topic_will = 'rfxcom2mqtt/status';
 const topic_info = 'rfxcom2mqtt/info';
@@ -12,6 +11,8 @@ const topic_command = 'rfxcom2mqtt/command';
 const topic_connected = 'rfxcom2mqtt/connected';
 
 console.log('RFXCOM2MQTT Starting...');
+
+console.log(config);
 
 const will = {"topic": topic_will, "payload": "offline", "retain": "true"}
 const options = {"will": will}
@@ -34,28 +35,7 @@ mqttClient.on('connect', () => {
   })
 })
 
-mqttClient.on('message', (topic_command, payload) => {
-  console.log('Send to RFXCOM: ', payload.toString())
-
-  const obj = JSON.parse(payload);
-  if (obj.type === "lighting2") {
-    const cmd = obj.command.split(" ")
-    if (cmd[0] === "on") {
-      lighting2.switchOn(obj.id);
-    } else if (cmd[0] === "off") {
-      lighting2.switchOff(obj.id);
-    } else if (cmd[0] === "level") {
-      lighting2.setLevel(obj.id, cmd[1]);
-    }
-  }
-  if (obj.type === "lighting4") {
-    lighting4.sendData(obj.id);
-  }
-  if (obj.type === "chime1") {
-    chime1.chime(obj.id);
-  }
-})
-
+// MQTT Connect
 mqttClient.on('connect', () => {
   mqttClient.publish(topic_will, 'online', { qos: 0, retain: true }, (error) => {
     if (error) {
@@ -78,40 +58,73 @@ const sendToMQTT = function(type, evt) {
       console.error(error)
     }
   })
-  console.log(json);
+  console.log("RFXCOM Receive:", json);
 }
 
-var rfxtrx = new rfxcom.RfxCom(device, {debug: true});
-var lighting2 = new rfxcom.Lighting2(rfxtrx, rfxcom.lighting2.AC);
+// RFXCOM Init
+var rfxdebug = (config.rfxcom.debug) ? config.rfxcom.debug : false;
+var rfxtrx = new rfxcom.RfxCom(config.rfxcom.usbport, {debug: rfxdebug});
+// TODO: transmit protocols
+// rfxcom.lighting2[evt.subtype]
+var lighting2 = new rfxcom.Lighting2(rfxtrx, rfxcom.lighting2['AC']);
 var lighting4 = new rfxcom.Lighting4(rfxtrx, rfxcom.lighting4.PT2262);
 var chime1 = new rfxcom.Chime1(rfxtrx, rfxcom.chime1.SELECT_PLUS);
 
 rfxtrx.initialise(function (error) {
   if (error) {
-    throw new Error("Unable to initialise the rfx device");
+    throw new Error("Unable to initialise the RFXCOM device");
   } else {
-    console.log("Device initialised");
+    console.log("RFXCOM device initialised");
   }
 });
 
-rfxtrx.on("lighting2", function (evt) {sendToMQTT("lighting2",evt)} );
-rfxtrx.on("lighting4", function (evt) {sendToMQTT("lighting4",evt)} );
-rfxtrx.on("temperaturehumidity1", function (evt) {sendToMQTT("temperaturehumidity1",evt)} );
+// RFXCOM Transmit
+mqttClient.on('message', (topic_command, payload) => {
+  console.log('RFXCOM Transmit:', payload.toString())
 
-function replacer(key, value) {
-  if (key === 'subtype' || key === 'seqnbr' || key === 'cmnd') {
-    return undefined;
+  const message = JSON.parse(payload);
+
+  const repeat = (config.rfxcom.transmit.repeat) ? config.rfxcom.transmit.repeat : 1
+  for (var i = 0; i < repeat; i++) {
+    if (message.type === "lighting2") {
+      const cmd = message.command.split(" ")
+      if (cmd[0] === "on") {
+        lighting2.switchOn(message.id);
+      } else if (cmd[0] === "off") {
+        lighting2.switchOff(message.id);
+      } else if (cmd[0] === "level") {
+        lighting2.setLevel(message.id, cmd[1]);
+      }
+    }
+    if (message.type === "lighting4") {
+      lighting4.sendData(message.id);
+    }
+    if (message.type === "chime1") {
+      chime1.chime(message.id);
+    }
   }
-  return value;
+})
+
+// RFXCOM Receive
+if (config.rfxcom.receive) {
+  config.rfxcom.receive.forEach(function(protocol) {
+    rfxtrx.on(protocol, function (evt) {sendToMQTT(protocol,evt)} );
+  });
 }
 
+// RFXCOM Status
 rfxtrx.on("status", function (evt) {
-  var json = JSON.stringify(evt, replacer, 2);
+  var json = JSON.stringify(evt, function(key, value) {
+    if (key === 'subtype' || key === 'seqnbr' || key === 'cmnd') {
+      return undefined;
+    }
+    return value;
+  }, 2);
 
   mqttClient.publish(topic_info, json, { qos: 0, retain: false }, (error) => {
     if (error) {
       console.error(error);
     }
   })
-  console.log(json);
+  console.log("RFXCOM Status:", json);
 });
